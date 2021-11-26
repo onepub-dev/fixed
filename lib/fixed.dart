a33385565ff7c0fbe7cfb203b2c807129ab67426
Brett Sutton <bsutton@noojee.com.au>
2021-11-27 08:09:58 +1100

    Update the library intro.

--- lib/fixed.dart
+++ lib/fixed.dart
@@ -1,6 +1,15 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// The Fixed package allows you to store and perform maths on decimal numbers
+/// with a fixed scale (fixed number of decimal places).
+/// All amounts are stored using the Decimal package to allow precision maths
+/// to be performed.
+/// 
+/// Conversion from a number of other numberic formats is supported
+/// as well as parsing and formatting Fixed scale no.s
+/// 
+/// Fixed uses the Decimal package to store the underlying values so
+/// the precision is only limited by the size of memory.
+/// 
+/// A range of mathematical comparision operations are supported.
 library fixed;
 
 export 'src/fixed.dart';
